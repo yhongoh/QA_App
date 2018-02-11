8b873441751ae232c062fbcfd6105224e7ecd3e7
y_hongoh <y_hongoh@me.com>
2018-02-11 15:41:38 +0900

    要件３対応中

--- app/src/main/java/jp/techacademy/hongou/yuka/qa_app/FavoriteActivity.java
+++ app/src/main/java/jp/techacademy/hongou/yuka/qa_app/FavoriteActivity.java
@@ -3,7 +3,6 @@ package jp.techacademy.hongou.yuka.qa_app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
